aad4773ac25cb7bb78f9cb081575305805ba607e
Thien <vo.thienhuongthi618@gmail.com>
2013-05-23 01:37:06 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
-
-  get "static_pages/contact"
+  
+  get "users/new"
+  match '/signup',  to: 'users#new', as: 'signup'
+  
+  match '/', to: 'static_pages#home', as: 'root'
+  match '/help',    to: 'static_pages#help', as: 'help'
+  match '/about',   to: 'static_pages#about', as: 'about'
+  match '/contact', to: 'static_pages#contact', as: 'contact'
+   
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
