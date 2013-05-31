079ad3ca0e675a37fbd61b75876ea1483ab6aeab
Thien <vo.thienhuongthi618@gmail.com>
2013-05-30 23:55:46 -0700

    Add delete for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController 
 	before_filter :signed_in_user, only: [:create, :destroy, :update]
+	before_filter :correct_user,   only: :destroy
 
 	def index
 	end 
@@ -18,5 +19,13 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
+		@post.destroy
+		redirect_to root_url
 	end
+
+	private
+		def correct_user
+			@post = current_user.posts.find_by_id(params[:id])
+			redirect_to root_url if @post.nil?
+		end
 end
